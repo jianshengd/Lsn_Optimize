a6a11eca6f357e21e1c5c2629ce786ebbb0840b7
jianshengd <jianshengd163@163.com>
2019-05-04 23:42:16 +0800

    Initial commit

---
+++ launch_optimize/src/main/java/com/jianshengd/launch_optimize/LaunchOptActivity.java
@@ -0,0 +1,13 @@
+package com.jianshengd.launch_optimize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LaunchOptActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lanch_opt);
+    }
+}
